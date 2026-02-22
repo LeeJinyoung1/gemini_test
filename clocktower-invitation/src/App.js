0ccba050d06fa2845b5888c519be88324661582f
Leejinyoung1 <adsl5964@naver.com>
2026-02-23 01:09:47 +0900

    feat: Preload invite image for better UX

--- clocktower-invitation/src/App.js
+++ clocktower-invitation/src/App.js
@@ -1,11 +1,17 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import FoldedInvitation from './components/FoldedInvitation';
 import UnfoldedInvitation from './components/UnfoldedInvitation';
+import inviteImg from './img/invite_img.jpg';
 
 function App() {
   const [isUnfolded, setIsUnfolded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = inviteImg;
+  }, []);
 
   const handleUnfold = () => {
     setIsUnfolded(true);
